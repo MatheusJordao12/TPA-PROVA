2306fea78162545867646645cec6c51fdc8076f4
Math200912 <matheusgames52626@gmail.com>
2025-11-24 09:55:52 -0300

    Comentários e dependências

--- automacao.py
+++ automacao.py
@@ -5,24 +5,31 @@ import keyboard
 
 tipo = input("Qual o tipo do cadastro? (excel ou web) ")
 
+# Define uma função chamada parar(), que interrompe o programa quando aperta o botão Insert (Ins)
 def parar():
     if keyboard.is_pressed("insert"):
         print("\nPrograma interrompido")
         exit()
+# Cada chamada de parar() verifica se o Insert foi apertado
 
+# Se a escolha for "excel"
 if tipo.lower() == "excel":
     excel = "excel.exe"
     processos = []
 
+    # Procura nos processos se o Excel está aberto
     for proc in psutil.process_iter(['name']):
         nome = proc.info['name']
         processos.append(nome)
         parar()
 
+    # Se o Excel estiver aberto, então vai para ele
     if excel in processos:
         pyautogui.hotkey('alt', 'tab')
         pyautogui.hotkey('enter')
-    else: 
+
+    # Se não estiver, abre o Excel
+    else:
         time.sleep(1)
         parar()
         pyautogui.hotkey('win')
@@ -32,6 +39,7 @@ if tipo.lower() == "excel":
         time.sleep(3)
         pyautogui.hotkey('enter')
 
+    # Depois disso, começa a digitar a tabela
     time.sleep(1)
     parar()
     pyautogui.write('Produto')
@@ -43,21 +51,23 @@ if tipo.lower() == "excel":
     pyautogui.write('Categoria')
     pyautogui.hotkey('enter')
 
-    with open("dados.txt", "r", encoding="utf-8") as arquivo:
-        linhas = arquivo.readlines()
+    # Lê o arquivo dados.txt
+    with open("dados.txt", "r", encoding="utf-8") as arquivo:  # "r" significa modo leitura com o enconding em utf-8 como arquivo
+        linhas = arquivo.readlines()  # Lê todas as linhas como uma lista
 
     for linha in linhas:
         parar()
-        partes = linha.strip().split(";")
+        partes = linha.strip().split(";")  # strip() remove espaços e \n ; split divide pelo ponto e vírgula fazendo uma lista
 
         for parte in partes:
             parar()
-            parte = parte.capitalize()
-            pyautogui.write(parte)
-            pyautogui.hotkey('tab')
+            parte = parte.capitalize()  # Deixa a primeira letra maiúscula
+            pyautogui.write(parte)     # Digita o texto
+            pyautogui.hotkey('tab')    # Vai para a próxima célula
 
-        pyautogui.hotkey('enter')
+        pyautogui.hotkey('enter')  # Vai para a próxima linha
 
+    # Ajusta o texto no Excel
     time.sleep(1)
     parar()
     pyautogui.hotkey('ctrl', 't')
@@ -69,6 +79,7 @@ if tipo.lower() == "excel":
     pyautogui.write('t')
 
 else:
+    # Caso não for excel, vai para o modo web
     pyautogui.hotkey('alt', 'tab')
 
     with open("dados.txt", "r", encoding="utf-8") as arquivo:
